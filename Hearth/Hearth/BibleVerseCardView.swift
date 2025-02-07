2838eec0997a100eff594a5344bc726ccce8fbcc
aaronmckain <almckain@ksu.edu>
2025-02-07 00:00:14 -0600

    Stylized MainView and added UI colors

---
+++ Hearth/Hearth/BibleVerseCardView.swift
@@ -0,0 +1,18 @@
+//
+//  BibleVerseCardView.swift
+//  Hearth
+//
+//  Created by Aaron McKain on 2/6/25.
+//
+
+import SwiftUI
+
+struct BibleVerseCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BibleVerseCardView()
+}
