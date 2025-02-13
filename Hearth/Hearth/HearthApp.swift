8eea3474ffe36a7061ea8d65546a37e628436efc
aaronmckain <almckain@ksu.edu>
2025-02-13 00:55:42 -0600

    Added Firebase  SDK

--- Hearth/Hearth/HearthApp.swift
+++ Hearth/Hearth/HearthApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import Firebase
 
 @main
 struct HearthApp: App {
@@ -14,6 +15,7 @@ struct HearthApp: App {
     
     // Testing purposes to control which screen is being presented
     init() {
+        FirebaseApp.configure()
         self.hasCompletedOnboarding = false
     }
     
